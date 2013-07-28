cf8e1cb96d76a29d5d464aba1589ae96bca193dd
Ubuntu <ubuntu@ip-172-31-19-61.us-west-2.compute.internal>
2013-07-28 23:43:34 +0000

    Added index.html to web.js

--- web.js
+++ web.js
@@ -5,10 +5,11 @@ var fs = require('fs');
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  response.send(fs.readFileSync('index.html'));	
+  var contents = fs.readFileSync('index.html');
+  response.send(contents);	
 });
 
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
