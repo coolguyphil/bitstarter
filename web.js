cb43572e16fefdfdc5a7682133d97ac84aeda105
Ubuntu <ubuntu@ip-172-31-19-61.us-west-2.compute.internal>
2013-07-29 02:52:18 +0000

    updated index.html

--- web.js
+++ web.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 var fs = require('fs');
+var htmlfile = "index.html";
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  var contents = fs.readFileSync('index.html', "utf-8");
-  response.send(contents.toString('utf8'));	
+  var contents = fs.readFileSync((htmlfile).toString();
+  response.send(contents);	
 });
 
 var port = process.env.PORT || 5000;
